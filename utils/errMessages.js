d0a348c692520a94b933300396a2728445233eed
Vu Truong Anh <truonganhvu205@gmail.com>
2025-12-30 14:01:40 +0700

    patch

---
+++ utils/errMessages.js
@@ -0,0 +1,15 @@
+const errMessages = {
+    login: 'The username or password you entered is incorrect.',
+
+    username_length: 'Username must be between 4 and 15 characters long.',
+    username_format: 'Username must start with a letter and may contain underscores (_).',
+    username_rule: 'Username cannot start or end with an underscore, and cannot contain consecutive underscores.',
+
+    password_length: 'Password must be at least 8 characters long.',
+    password_format: 'Password must contain at least one letter, one number, or one special character.',
+    password_match: 'Passwords do not match.',
+
+    email: 'Please enter a valid email address.',
+}
+
+export default errMessages
